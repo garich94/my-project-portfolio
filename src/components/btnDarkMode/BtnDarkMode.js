c109bda3368c33edb98ca9677ecc4a2238cb25f4
garich94 <114189622+garich94@users.noreply.github.com>
2023-02-27 23:42:38 +0300

    1

--- src/components/btnDarkMode/BtnDarkMode.js
+++ src/components/btnDarkMode/BtnDarkMode.js
@@ -1,21 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef } from "react";
+import { useLocalStorage } from "../../utils/useLocalStorage";
 
 import sun from "./sun.svg";
 import moon from "./moon.svg";
 
 import "./style.css";
+import detectDarkMode from "../../utils/detectDarkMode";
 
 const BtnDarkMode = () => {
-  const [darkMode, setDarkMode] = useState("light");
+  const [darkMode, setDarkMode] = useLocalStorage("darkMode", detectDarkMode);
+
+  const btnRef = useRef(null);
+
+  useEffect(() => {
+    if (darkMode === "dark") {
+      document.body.classList.add("dark");
+      btnRef.current.classList.add("dark-mode-btn--active");
+    } else {
+      document.body.classList.remove("dark");
+      btnRef.current.classList.remove("dark-mode-btn--active");
+    }
+  }, [darkMode]);
 
   const togleDarkMode = () => {
-    setDarkMode(() => {
-      return darkMode === "light" ? "dark" : "light";
+    setDarkMode((currentValue) => {
+      return currentValue === "light" ? "dark" : "light";
     });
   };
-
   return (
-    <button className="dark-mode-btn" onClick={togleDarkMode}>
+    <button ref={btnRef} className="dark-mode-btn" onClick={togleDarkMode}>
       <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
       <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
     </button>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,71 +5,7 @@ const Home = () => {
     <>
       <Header />
       <main className="section">
-        <div className="container">
-          <h2 className="title-1">Projects</h2>
-          <ul className="projects">
-            <li className="project">
-              <a href="project-page.html">
-                <img
-                  src="./img/projects/01.jpg"
-                  alt="Project img"
-                  className="project__img"
-                />
-                <h3 className="project__title">Gaming streaming portal</h3>
-              </a>
-            </li>
-            <li className="project">
-              <a href="project-page.html">
-                <img
-                  src="./img/projects/02.jpg"
-                  alt="Project img"
-                  className="project__img"
-                />
-                <h3 className="project__title">Video service</h3>
-              </a>
-            </li>
-            <li className="project">
-              <a href="project-page.html">
-                <img
-                  src="./img/projects/03.jpg"
-                  alt="Project img"
-                  className="project__img"
-                />
-                <h3 className="project__title">Video portal</h3>
-              </a>
-            </li>
-            <li className="project">
-              <a href="project-page.html">
-                <img
-                  src="./img/projects/04.jpg"
-                  alt="Project img"
-                  className="project__img"
-                />
-                <h3 className="project__title">Dating app</h3>
-              </a>
-            </li>
-            <li className="project">
-              <a href="project-page.html">
-                <img
-                  src="./img/projects/05.jpg"
-                  alt="Project img"
-                  className="project__img"
-                />
-                <h3 className="project__title">Landing</h3>
-              </a>
-            </li>
-            <li className="project">
-              <a href="project-page.html">
-                <img
-                  src="./img/projects/06.jpg"
-                  alt="Project img"
-                  className="project__img"
-                />
-                <h3 className="project__title">Gaming community</h3>
-              </a>
-            </li>
-          </ul>
-        </div>
+        <div className="container"></div>
       </main>
     </>
   );
