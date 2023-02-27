7a6a1511c1a49eb7a7f474b4fd67c1fbbc99f6eb
garich94 <114189622+garich94@users.noreply.github.com>
2023-02-27 23:48:34 +0300

    2

--- src/components/btnDarkMode/BtnDarkMode.js
+++ src/components/btnDarkMode/BtnDarkMode.js
@@ -22,6 +22,15 @@ const BtnDarkMode = () => {
     }
   }, [darkMode]);
 
+  useEffect(() => {
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", (event) => {
+        const newColorScheme = event.matches ? "dark" : "light";
+        setDarkMode(newColorScheme);
+      });
+  }, []);
+
   const togleDarkMode = () => {
     setDarkMode((currentValue) => {
       return currentValue === "light" ? "dark" : "light";
