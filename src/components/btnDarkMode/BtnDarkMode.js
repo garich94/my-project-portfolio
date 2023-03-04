b1f49c6a0ef7bbd7563a7f1e0d1c26af2487dd8f
garich94 <114189622+garich94@users.noreply.github.com>
2023-03-04 12:50:16 +0300

    1

--- src/components/btnDarkMode/BtnDarkMode.js
+++ src/components/btnDarkMode/BtnDarkMode.js
@@ -4,6 +4,8 @@ import { useLocalStorage } from "../../utils/useLocalStorage";
 import sun from "./sun.svg";
 import moon from "./moon.svg";
 
+import Home from "./../../pages/Home";
+
 import "./style.css";
 import detectDarkMode from "../../utils/detectDarkMode";
 
@@ -36,15 +38,19 @@ const BtnDarkMode = () => {
   const btnNormal = "dark-mode-btn";
   const btnActive = "dark-mode-btn dark-mode-btn--active";
 
-  return (
-    <button
-      className={darkMode === "dark" ? btnActive : btnNormal}
-      onClick={togleDarkMode}
-    >
-      <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
-      <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
-    </button>
-  );
+  if (Home === true) {
+    return null;
+  } else {
+    return (
+      <button
+        className={darkMode === "dark" ? btnActive : btnNormal}
+        onClick={togleDarkMode}
+      >
+        <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
+        <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
+      </button>
+    );
+  }
 };
 
 export default BtnDarkMode;
